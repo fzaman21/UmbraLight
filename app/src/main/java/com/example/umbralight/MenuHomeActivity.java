8380d843896476ae2b5f7f15eaf1fc8147f2343e
Fikih <zamanFikih21@gmail.com>
2019-07-07 20:53:42 +0700

    add menu home

---
+++ app/src/main/java/com/example/umbralight/MenuHomeActivity.java
@@ -0,0 +1,22 @@
+package com.example.umbralight;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MenuHomeActivity extends Fragment {
+    View v;
+
+    public MenuHomeActivity() {}
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        v = inflater.inflate(R.layout.activity_menu_home, container, false);
+        return v;
+    }
+}
