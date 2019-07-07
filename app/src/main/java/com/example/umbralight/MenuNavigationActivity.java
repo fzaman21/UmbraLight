79f51eb62fe7734a42531adbe44a531eff8372c8
Fikih <zamanFikih21@gmail.com>
2019-07-07 20:58:08 +0700

    add menu navigation

---
+++ app/src/main/java/com/example/umbralight/MenuNavigationActivity.java
@@ -0,0 +1,24 @@
+package com.example.umbralight;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+public class MenuNavigationActivity extends Fragment {
+    public MenuNavigationActivity(){}
+    RelativeLayout view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        view = (RelativeLayout) inflater.inflate(R.layout.activity_menu_navigation, container, false);
+
+        getActivity().setTitle("Gallery");
+
+        return view;
+    }
+}
