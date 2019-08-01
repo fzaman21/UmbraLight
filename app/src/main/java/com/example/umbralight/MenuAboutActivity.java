c5a656a2044ecad85b902c10adc564b2e30199c4
Fikih Zaman <zamanFikih21@gmail.com>
2019-08-02 00:17:37 +0700

    add about

--- app/src/main/java/com/example/umbralight/MenuAboutActivity.java
+++ app/src/main/java/com/example/umbralight/MenuAboutActivity.java
@@ -9,18 +9,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 public class MenuAboutActivity extends Fragment {
     public MenuAboutActivity(){}
-    RelativeLayout view;
+    LinearLayout view;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        view = (RelativeLayout) inflater.inflate(R.layout.activity_menu_about, container, false);
+        view = (LinearLayout) inflater.inflate(R.layout.activity_menu_about, container, false);
 
         getActivity().setTitle("About");
 
